913481f0949a612409966ec3bb2da357f27e9ff7
Lukian <lukian55d@gmail.com>
2025-05-02 10:52:45 +0100

    donnut

--- app/main.py
+++ app/main.py
@@ -2,5 +2,12 @@ from typing import Callable
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    stored_res = {}
+    def wrapper(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key in stored_res:
+            return stored_res[key]
+        res = func(*args, **kwargs)
+        stored_res[key] = res
+        return res
+    return wrapper
